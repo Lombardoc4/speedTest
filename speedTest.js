897a841b297c2c91cf78c7e0d299f89471d63a9c
Cris Lom <bigc4997@gmail.com>
2021-02-03 08:06:51 -0500

    added elements working on functionality

--- speedTest.js
+++ speedTest.js
@@ -1,14 +1,26 @@
+const $maxIterations = document.getElementById('maxIterations');
+const $maxTime = document.getElementById('maxTime');
 
-const $maxCountInput = document.getElementById('maxCount');
 const iterationOptions = [1000, 100000, 1000000, 100000000];
+const timeOptions = [1000, 10000, 30000];
 
-const exArray = [10, 100, 1000, 1000, 1000];
 
-// Function 1
-const reduceArray = () => exArray.reduce((x) => x + 1);
+document.getElementById('runTest').addEventListener('submit', (e) => { e.preventDefault(); });
 
-// Function 2
 
+
+
+const setTimeOptions = (timeOptions) => {
+    let i = 0;
+    while (i < timeOptions.length) {
+        const newOption = document.createElement('option');
+        newOption.setAttribute('value', timeOptions[i]);
+        newOption.innerHTML = `${+timeOptions[i] / 1000} second/s`;
+
+        $maxTime.append(newOption);
+        i += 1;
+    }
+};
 
 const setIterationOptions = (iterationOptions) => {
     let i = 0;
@@ -17,21 +29,74 @@ const setIterationOptions = (iterationOptions) => {
         newOption.setAttribute('value', iterationOptions[i]);
         newOption.innerHTML = iterationOptions[i];
 
-        $maxCountInput.append(newOption);
+        $maxIterations.append(newOption);
         i += 1;
     }
 };
 
-setIterationOptions(iterationOptions);
+const showOptions = (value) => {
+    console.log(value);
+    const limitRadios = document.getElementsByName('limitOptions');
+    let j = 0;
+    while (j < limitRadios.length) {
+        if (limitRadios[i].id === value)
+            limitRadioss[j].style.display = 'block';
+        else
+            limitRadioss[j].style.display = 'none';
 
-const runSpeedTest = (testFunc) => {
-    const t0 = performance.now();
-    for (let n = 0; n < $maxCountInput.value; n++)
-        testFunc();
-    const t1 = performance.now();
-    console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
-    // console.time('My operation');
-    // console.timeEnd('My operation');
+        j += 1;
+    }
+
+
 };
 
-runSpeedTest(reduceArray);
+setIterationOptions(iterationOptions);
+setTimeOptions(timeOptions);
+
+// Radio Inputs
+const limitRadios = document.getElementsByName('limit');
+let i = 0;
+while (i < limitRadios.length) {
+    limitRadios[i].addEventListener('change', function() {
+        // limitRadios.
+        // limitRadios.querySelector('[checked]');
+        // console.log('this',this);
+        if (this.hasAttribute('checked'))
+
+            showOptions(this.value);
+    });
+
+    if (limitRadios[i].hasAttribute('checked')){
+        // limitRadios[i].value
+    }
+
+    // const newOption = document.createElement('option');
+    // newOption.setAttribute('value', iterationOptions[i]);
+    // newOption.innerHTML = iterationOptions[i];
+
+    // $maxIterations.append(newOption);
+    i += 1;
+}
+
+// Limit Inputs
+const limitOptions = document.getElementsByClassName('limitOptions');
+let j = 0;
+while (j < limitOptions.length) {
+    // console.log(limitOptions[j]);
+    limitOptions[j].style.display = 'none';
+    j += 1;
+}
+
+
+
+// const runSpeedTest = (testFunc) => {
+//     const t0 = performance.now();
+//     for (let n = 0; n < $maxIterations.value; n++)
+//         testFunc();
+//     const t1 = performance.now();
+//     console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
+//     // console.time('My operation');
+//     // console.timeEnd('My operation');
+// };
+
+// runSpeedTest(reduceArray);
